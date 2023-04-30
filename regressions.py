4a7e8b12314f6cca74c3c28f8cf84b35f157a779
idrismi <65001205+idrismi@users.noreply.github.com>
2023-04-30 19:58:41 +0800

    Update regressions.py

--- regressions.py
+++ regressions.py
@@ -7,51 +7,58 @@ class LinearRegression:
         self.y = y
         self.x_mean = np.mean(x)
         self.y_mean = np.mean(y)
-        self.gradient = self.get_gradient()
-        self.constant = self.get_constant()
+        self.gradient = self._get_gradient()
+        self.constant = self._get_constant()
 
-    def get_gradient(self):
-        '''Calculates the gradient of a linear regression line.''' 
-        numerator = sum((self.x[i] - self.x_mean) * (self.y[i] - self.y_mean) for i in range(len(self.x)))
-        denominator = sum((self.x[i] - self.x_mean)**2 for i in range(len(self.x)))
+    def _get_gradient(self):
+        """
+        Calculates the gradient of a linear regression line.
+        """
+        numerator = sum(
+            (self.x[i] - self.x_mean) * (self.y[i] - self.y_mean)
+            for i in range(len(self.x))
+        )
+        denominator = sum((self.x[i] - self.x_mean) ** 2 for i in range(len(self.x)))
         return numerator / denominator
 
-    def get_constant(self):
-        """Calculates the y-intercept of the linear regression line."""
+    def _get_constant(self):
+        """
+        Calculates the y-intercept of the linear regression line.
+        """
         return self.y_mean - self.gradient * self.x_mean
 
-    def expected_y(self):
+    def _expected_y(self):
         """
         Returns the y values when the actual x values are
         entered into the linear regression equation.
+            y = gradient * x + constant
         """
-        # y = gradient * x + constant
         return self.gradient * self.x + self.constant
 
-    def expected_x(self):
+    def _expected_x(self):
         """
         Returns the x values when the actual y values are
         entered into the linear regression equation.
+            x = (y - constant) / gradient
         """
-        # x = (y - constant) / gradient
         return (self.y - self.constant) / self.gradient
 
     def r_squared(self):
         """
-        Calculates R squared to see how well the regression 
+        Calculates R squared to see how well the regression
         line fits the data.
         Ranges from 0 - 1
         The closer to 1 the better the fit.
         """
         # Equation from: https://www.khanacademy.org/math/statistics-probability/describing-relationships-quantitative-data/more-on-regression/v/calculating-r-squared
-        sum_squared_error = sum((self.expected_y() - self.y) ** 2)
+        sum_squared_error = sum((self._expected_y() - self.y) ** 2)
         sum_squared_dist_from_mean = sum((self.y - self.y_mean) ** 2)
         return 1 - (sum_squared_error / sum_squared_dist_from_mean)
 
 
 class ExponentialRegression:
     """
-    Find linear regression equation and convert back to 
+    Find linear regression equation and convert back to
     exponential equation.
     y = a * b**t => exponential standard equation.
     a = y-intercept
@@ -82,7 +89,8 @@ class ExponentialRegression:
         """
         return np.e ** self.m - 1
 
-    def future_value(self, year):
-        
-        return np.e * (self.m * year + self.c)
-
+    def expected_y(self, x):
+        """
+        Calculates y based on a given x value
+        """
+        return np.e * (self.m * x + self.c)
